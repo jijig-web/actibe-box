e1e556489af9b8bceec64d152293451cad43582b
jijig-web <64476618+jijig-web@users.noreply.github.com>
2021-05-26 00:21:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,26 @@ function openMenu() {
 
     menu.classList.toggle('open')
 }
+
+ $slick_slider = $('.slider');
+  settings_slider = {
+    arrows: true
+    }
+
+  slick_on_mobile( $slick_slider, settings_slider);
+
+// slick on mobile
+  function slick_on_mobile(slider, settings){
+    $(window).on('load resize', function() {
+      if ($(window).width() > 600) {
+        if (slider.hasClass('slick-initialized')) {
+          slider.slick('unslick');
+        }
+        return
+      }
+      if (!slider.hasClass('slick-initialized')) {
+        return slider.slick(settings);
+      }
+    });
+  };
+
